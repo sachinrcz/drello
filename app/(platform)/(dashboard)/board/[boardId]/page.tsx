dd8f9d840b332b59dc8bed72f34ec5da88ccabba
Sachin <sachin_rcz@yahoo.co.in>
2024-01-11 20:55:00 +0800

    list component

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,9 +1,48 @@
-const BoardIdPage = () => {
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+    params: {
+        boardId: string;
+    };
+};
+
+
+const BoardIdPage = async ({params}: BoardIdPageProps) => {
+
+    const {orgId} = auth();
+
+    if (!orgId){
+        redirect("/select-org")
+    }
+
+    const lists = await db.list.findMany({
+        where:{
+            boardId: params.boardId,
+            board: {
+                orgId
+            }
+        },
+        include: {
+            card:{
+                orderBy:{
+                    order: "asc",
+                }
+            }
+        },
+        orderBy: {
+            order: "asc"
+        }
+    });
+
     return (
-        <div 
-            
-        >
-            Board ID!
+        <div className="p-4 h-full overflow-x-auto">
+            <ListContainer
+                boardId = {params.boardId}
+                data={lists}
+            />
         </div>
     )
 };
